ea621ddd0724ffc376123768f88189eb4123ecab
hassan <hr033469@gmail.com>
2024-02-02 14:14:21 +0200

    add Availability

--- NewWorker/src/App.js
+++ NewWorker/src/App.js
@@ -5,23 +5,29 @@ import Layout from "./Components/Layout/Layout";
 import WorkerReg from "./Components/WorkerReg/WorkerReg";
 import ServicReg from "./Components/ServiceReg/ServiceReg";
 import Landpage from "./Components/Landpage/Landpage";
-import Table from './Components/Tables/Table';
+import Table from "./Components/Tables/Table";
 import ReqDetails from "./Components/ReqDetails/ReqDetails";
+import Availability from "./Components/AddAvailability/Availability";
 
 let Routers = createBrowserRouter([
-  {path:'/',element:<Layout/>, children:[
-    {index:true,element:<WorkerReg/>},
-    {path:'/serviceReg',element:<ServicReg/>},
-    {path:'/landpage',element:<Landpage/>},
-    {path:'/table',element:<Table/>},
-    {path:'/reqdetail',element:<ReqDetails/>}
-  ]}
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <WorkerReg /> },
+      { path: "/serviceReg", element: <ServicReg /> },
+      { path: "/landpage", element: <Landpage /> },
+      { path: "/table", element: <Table /> },
+      { path: "/addAvailability", element: <Availability /> },
+      { path: "/reqdetail", element: <ReqDetails /> },
+    ],
+  },
+]);
 function App() {
   return (
-   <>
-   <RouterProvider router={Routers}></RouterProvider>
-   </>
+    <>
+      <RouterProvider router={Routers}></RouterProvider>
+    </>
   );
 }
 
--- NewWorker/src/index.js
+++ NewWorker/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-//import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
